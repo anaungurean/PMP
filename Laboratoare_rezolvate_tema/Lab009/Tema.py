73959ac0e82e7a348e4dfff7982b402cbc8c57c2
anaungurean <100145653+anaungurean@users.noreply.github.com>
2023-12-05 15:13:21 +0200

    -add-tema-9

--- Laboratoare_rezolvate_tema/Lab009/Tema.py
+++ Laboratoare_rezolvate_tema/Lab009/Tema.py
@@ -2,31 +2,55 @@ import pandas as pd
 import numpy as np
 import pymc3 as pm
 import matplotlib.pyplot as plt
+import arviz as az
+
+rng = np.random.default_rng(100)
+
+def plot_gre_gpa(idata, gre, gpa):
+    beta0 = idata.posterior['beta_0'].mean(dim=('chain', 'draw'))
+    beta = idata.posterior['beta'].mean(dim=('chain', 'draw'))
+
+    probabilities = 1 / (1 + np.exp(-(beta0 + gre * beta[0] + gpa * beta[1])))
+
+    hdi = pm.stats.hdi(probabilities, hdi_prob=0.9)
+    hdi_probabilities = probabilities[(probabilities >= hdi[0]) & (probabilities <= hdi[1])]
+
+    plt.plot(np.sort(hdi_probabilities))
+    plt.title(f'GRE = {gre} GPA = {gpa}')
+    plt.show()
 
 def main():
     data = pd.read_csv("Admission.csv")
-
-    gre_scores = data["GRE"].values
-    gpa_scores = data["GPA"].values
+    scores = data[['GRE', 'GPA']].values
     admission_status = data['Admission'].values
+    scores = np.array(scores)
 
     with pm.Model() as logistic_model:
-        beta_0 = pm.Normal('beta_0', mu=0, sigma=10)
-        beta_1 = pm.Normal('beta_1', mu=0, sigma=10)
-        beta_2 = pm.Normal('beta_2', mu=0, sigma=10)
+        beta_0 = pm.Normal('beta_0', mu=0, sd=10)
+        beta = pm.Normal('beta', mu=0, sd=2, shape=2)
 
-        pi = pm.math.invlogit(beta_0 + beta_1 * gre_scores + beta_2 * gpa_scores)
-        admission_likelihood = pm.Bernoulli('admission_likelihood', p=pi, observed=admission_status)
+        mu = beta_0 + pm.math.dot(scores, beta)
 
-    with logistic_model:
-        trace = pm.sample(1000, tune=1000, cores=2)
+        theta = pm.Deterministic('theta', 1 / (1 + pm.math.exp(-mu)))
+        bd = pm.Deterministic('bd', -beta_0 / beta[1] - beta[0] / beta[1] * scores[:, 0])
+        yl = pm.Bernoulli('yl', p=theta, observed=admission_status)
+        idata = pm.sample(2000, random_seed=100, return_inferencedata=True)
 
-    beta_0_samples = trace['beta_0']
-    beta_1_samples = trace['beta_1']
-    beta_2_samples = trace['beta_2']
+    idx = np.argsort(scores[:, 0])
+    bd = idata.posterior['bd'].mean(dim=('chain', 'draw'))[idx]
+    print(bd.mean())
 
-    decision_boundary = np.median(pm.math.invlogit(beta_0_samples + beta_1_samples * gre_scores + beta_2_samples * gpa_scores))
+    plt.scatter(scores[:, 0], scores[:, 1], c=admission_status)
+    plt.plot(scores[:, 0][idx], bd, color='k')
+    az.plot_hdi(scores[:, 0], idata.posterior['bd'], color='k')
+    plt.xlabel('GRE')
+    plt.ylabel('GPA')
+    plt.title('Admission')
+    plt.show()
 
+    plot_gre_gpa(idata, 3.5, 550)
+    plot_gre_gpa(idata, 3.2, 500)
+    plot_gre_gpa(idata, 4.5, 1000)
 
-if "__name__" == "__main__":
+if __name__ == "__main__":
     main()
